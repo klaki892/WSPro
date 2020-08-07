8389e47858ea626ee7a885db7b09593871ee1a94
Klayton Killough <klaki892@gmail.com>
2020-08-07 16:34:44 -0400

    Add Copyright Notices

--- core/src/test/java/scripting/FactoryObjectTest.java
+++ core/src/test/java/scripting/FactoryObjectTest.java
@@ -1,7 +1,22 @@
-package scripting;
+/*******************************************************************************
+ *  WSPro - Trading Card Game Simulator
+ *  Copyright (C) 2020  Klayton Killough
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, version 3 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+package scripting;
 
 public class FactoryObjectTest {
 
--- desktop-client/src/main/java/ton/klay/weebsports/swingfrontend/ShapeMover.java
+++ desktop-client/src/main/java/ton/klay/weebsports/swingfrontend/ShapeMover.java
@@ -1,13 +1,25 @@
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Rectangle;
+/*******************************************************************************
+ *  WSPro - Trading Card Game Simulator
+ *  Copyright (C) 2020  Klayton Killough
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, version 3 of the License.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ ******************************************************************************/
+
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
 
 public class ShapeMover {
 
